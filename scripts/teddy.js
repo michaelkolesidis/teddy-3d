f295a8ec86a0ce52857f954e573565520d78540d
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-05-04 15:52:19 +0300

    console.log background color

--- scripts/teddy.js
+++ scripts/teddy.js
@@ -1,5 +1,5 @@
-let teddy;
-let teddyColor;
+let teddy; // The 3D model of the teddy bear;
+let teddyColor; // The color of the teddy bear;
 let colored = false;
 let backgroundColor;
 
@@ -16,37 +16,45 @@ function setup() {
 function draw() {
   background(backgroundColor);
 
+  // Anti-aliasing
   smooth();
+
+  // Teddy orbit controlled with mouse along with its rotation
+  orbitControl();
 
   // Scaled to make model fit into canvas
   scale(window.innerHeight / 450);
 
-  noStroke();
-
+  // Lights
   ambientLight(108, 108, 108);
   directionalLight(128, 128, 128, 0, 0, -1);
 
+  // Initial teddy color
   normalMaterial();
 
   if (colored) {
     fill(teddyColor);
   }
 
+  // Teddy rotation
   rotateX(PI / 1.2 + frameCount * 0.01);
   rotateY(PI / 1.2 + frameCount * 0.01);
 
+  // The 3D model of the teddy bear
   model(teddy);
 }
 
 function colorize() {
   teddyColor = color(random(40, 255), random(40, 255), random(40, 255));
   backgroundColor = color(random(40), random(40), random(40));
-  
+
   console.log(
-    `%cTeddy color: ${teddyColor}`,
+    `%cTeddy color: ${teddyColor}\nBackground color: ${backgroundColor}`,
     `color: ${teddyColor};
+     background: ${backgroundColor};
      font-weight: 600;`
   );
+
   colored = true;
 }
 
